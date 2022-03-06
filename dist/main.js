3860724cbb555f3890faaf0763d8deb430fecbd0
math0898 <25396616+math0898@users.noreply.github.com>
2022-03-05 19:06:11 -0600

    Create Miner.js

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var main = {};
+
+var loop = main.loop = function () {
+    console.log('Hello World!');
+};
+
+exports["default"] = main;
+exports.loop = loop;
+//# sourceMappingURL=main.js.map
---
+++ dist/main.js.map.js
@@ -0,0 +1,1 @@
+module.exports = {"version":3,"file":"main.js","sources":["../src/main.js"],"names":[],"mappings":";;;;;;oBACmB,GAAG,YAAY;AAClC,IAAI,OAAO,CAAC,GAAG,CAAC,cAAc,CAAC,CAAC;AAChC;;;;;"};
