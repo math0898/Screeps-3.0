8c75e45a9ac963c4a56f535afd2c46260236c871
math0898 <25396616+math0898@users.noreply.github.com>
2022-08-27 21:35:25 -0500

    Started Visuals - Started working on a general stats room visual.

--- dist/main.js
+++ dist/main.js
@@ -10,10 +10,37 @@ class SmartCreep {
     /**
      * Creates a new SmartCreep with the given Creep reference.
      * 
-     * @param {Creep} c The creep object of this SmartCreep. 
+     * @param {string} c The name of the Creep object of this SmartCreep. 
      */
-     constructor (c) {
-        this.creep = Game.creeps[c];
+    constructor (c) {
+        this.creepName = c;
+    }
+
+    /**
+     * Announces the role of this creep to creep.say().
+     */
+     announceRole () {
+        this.getCreep().say("⚙" + this.getCreep().memory.role);
+    }
+    
+    /**
+     * Grabs the current version of the attached creep from the list of game objects.
+     * 
+     * @return {Creep} The creep object associated with this SmartCreep.
+     */
+    getCreep () {
+        return Game.creeps[creepName];
+    }
+
+    /**
+     * Returns the body that should be attached to this creep given the estimated move distance and available energy.
+     * 
+     * @param {Number} dis      The expected number of tiles this creep may need to walk.
+     * @param {Number} capacity The amount of energy that can be spent on this creep.
+     * @return {Number[]} The best body for this task with the given information.
+     */
+    getBody (dis, capacity) {
+        return [ MOVE ];
     }
 }
 
@@ -25,12 +52,6 @@ class SmartCreep {
  */
 class EconomicCreep extends SmartCreep {
 
-    /**
-     * Announces the role of this creep to creep.say().
-     */
-    announceRole () {
-        this.creep.say("Hello World!");
-    }
 }
 
 /**
@@ -41,11 +62,11 @@ class EconomicCreep extends SmartCreep {
 class Miner extends EconomicCreep {
 
     /**
-     * Generates the body for a Miner creep with the given
-     * distance to a source and maximum capacity.
+     * Returns the body that should be attached to this creep given the estimated move distance and available energy.
      * 
-     * @param {Number} dis      The number of tiles to get to the source.
+     * @param {Number} dis      The expected number of tiles this creep may need to walk.
      * @param {Number} capacity The amount of energy that can be spent on this creep.
+     * @return {Number[]} The best body for this task with the given information.
      */
     getBody (dis, capacity) {
         if (capacity <= 300 && dis <= 100) return [ WORK, MOVE, WORK, WORK ];  
@@ -57,6 +78,7 @@ console.log("Code Refreshed");
 require('lodash');
 
 var rooms;
+var visuals = new Array();
 
 /**
  * Attempts to locate all the rooms currently under this bot's control. They are then placed into a helpful array to
@@ -76,12 +98,33 @@ function initRooms () {
  * 
  * @author Sugaku
  */
-global.Report = module.exports = {
+global.Report = {
+
+    /**
+     * Creates a visual display of current information.
+     * 
+     * @param {String} room The room to display the visuals for.
+     * @returns 0 on success.
+     */ // TODO: Map visuals
+    visualize (room) {
+        let roomVis = new RoomVisual(room);
+        roomVis.rect(new RoomPosition(0, 0, room), 49, 1, {fill: "#000000", opacity: 0.5})
+                .text("-=CPU STATS=-", 4.5, 0.75, {color: '#ffffff', opacity: 0.8})
+                .rect(new RoomPosition(1, 1, room), 7, 1, {fill: '#5b5b5b', opacity: 0.5})
+                .rect(new RoomPosition(1, 1, room), Game.cpu.getUsed() / 14.28, 1, {fill: '#ffffff', opacity: 0.8})
+                .text("CPU: " + Math.round(Game.cpu.getUsed()) + " / " + Game.cpu.limit, 4.5, 1.75, 
+                        {color: '#000000', opacity: 0.8})
+                .rect(new RoomPosition(1, 2, room), 7, 1, {fill: '#5b5b5b', opacity: 0.5})
+                .rect(new RoomPosition(1, 2, room), Game.cpu.bucket / 1428, 1, {fill: '#ffffff', opacity: 0.8})
+                .text("BUCKET: " + Game.cpu.bucket, 4.5, 2.75, {color: '#000000', opacity: 0.8});
+        visuals[room] = roomVis.export();
+        return 0;
+    },
 
     /**
      * Lists all currently visible and controlled rooms in a readable format.
      * 
-     * @returns 0 on success
+     * @returns 0 on success.
      */
     rooms () {
         console.log("Visible Rooms:");
@@ -101,6 +144,10 @@ module.exports.loop = function () {
     for (let c in Game.creeps) {
         var m = new Miner(c);
         m.announceRole();
+    }
+    for (let v in visuals) {
+        if (v == "map") Game.map.visual.import(v);
+        else global.Report.visualize(v);
     }
 };
 //# sourceMappingURL=main.js.map
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ let _ = require('lodash');
 import { Miner } from "./creeps/economy/Miner";
 
 var rooms;
+var visuals = new Array();
 
 /**
  * Attempts to locate all the rooms currently under this bot's control. They are then placed into a helpful array to
@@ -22,21 +23,27 @@ function initRooms () {
  * 
  * @author Sugaku
  */
-global.Report = module.exports = {
-
-    /**
-     * Creates a visual display of current information.
-     */
+global.Report = {
 
     /**
      * Creates a visual display of current information.
      * 
-     * @param {Number} ticks The number of ticks to display the visuals for.
+     * @param {String} room The room to display the visuals for.
      * @returns 0 on success.
-     */
-    visualize (ticks) {
-        this.displayTicks = ticks;
-        Game.map.visual.rect({x: 0, y: 0}, 50, 1, {fill: "#ff000", opacity: 0.2});
+     */ // TODO: Map visuals
+    visualize (room) {
+        let roomVis = new RoomVisual(room);
+        roomVis.rect(new RoomPosition(0, 0, room), 49, 1, {fill: "#000000", opacity: 0.5})
+                .text("-=CPU STATS=-", 4.5, 0.75, {color: '#ffffff', opacity: 0.8})
+                .rect(new RoomPosition(1, 1, room), 7, 1, {fill: '#5b5b5b', opacity: 0.5})
+                .rect(new RoomPosition(1, 1, room), Game.cpu.getUsed() / 14.28, 1, {fill: '#ffffff', opacity: 0.8})
+                .text("CPU: " + Math.round(Game.cpu.getUsed()) + " / " + Game.cpu.limit, 4.5, 1.75, 
+                        {color: '#000000', opacity: 0.8})
+                .rect(new RoomPosition(1, 2, room), 7, 1, {fill: '#5b5b5b', opacity: 0.5})
+                .rect(new RoomPosition(1, 2, room), Game.cpu.bucket / 1428, 1, {fill: '#ffffff', opacity: 0.8})
+                .text("BUCKET: " + Game.cpu.bucket, 4.5, 2.75, {color: '#000000', opacity: 0.8});
+        visuals[room] = roomVis.export();
+        return 0;
     },
 
     /**
@@ -62,5 +69,9 @@ module.exports.loop = function () {
     for (let c in Game.creeps) {
         var m = new Miner(c);
         m.announceRole();
+    }
+    for (let v in visuals) {
+        if (v == "map") Game.map.visual.import(v);
+        else global.Report.visualize(v);
     }
 }
